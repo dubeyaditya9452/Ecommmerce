fdbc42373dcf157f1892bc0ade03e18f5f38a017
Aditya <dubeyaditya80045@gmail.com>
2023-08-21 22:11:05 +0530

    Added cheatsheet

--- script.js
+++ script.js
@@ -83,3 +83,33 @@ let aboutus = document.querySelector('.about');
     document.querySelector('.crd').style.display ="none";
     Order.style.display ='block';
   }
+
+
+  const  nav =document.querySelector('nav');
+  const navOffset= nav.offsetTop;
+
+
+    function toggleSticky() {
+        if (window.pageYOffset >= navOffset) {
+          nav.classList.add('sticky');
+        } else {
+          nav.classList.remove('sticky');
+        }
+      }
+window.addEventListener('scroll', toggleSticky);
+
+function scrollToTop(){
+  window.scrollTo({
+   top:0,
+   behavior:"smooth"
+  });
+}
+
+window.onscroll = function() {
+  var scrollToTopBtn = document.getElementById("#scroll-to-top-btn");
+  if (window.scrollY >= 300) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}; 
